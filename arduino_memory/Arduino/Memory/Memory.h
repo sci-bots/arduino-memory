9bfeaefbf98478c645751fed75c19d12f70f1303
Christian Fobel <christian@fobel.net>
2016-04-26 07:15:09 -0400

    Add free_memory support for Teensy 3.x
    See [here][1] for information about the Teensy platform.
    
    [1]: https://www.pjrc.com/store/teensy32.html

--- arduino_memory/Arduino/Memory/Memory.h
+++ arduino_memory/Arduino/Memory/Memory.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2013 Ryan Fobel
+Copyright 2013-2016 Ryan Fobel and Christian Fobel
 
 This file is part of the Arduino Memory library.
 
@@ -22,7 +22,11 @@ along with the Arduino Memory Library.  If not, see <http://www.gnu.org/licenses
 
 #include <stdint.h>
 #include <stdlib.h>
+
+#ifdef __AVR__
 #include <avr/io.h> //RAMEND, SP
+typedef uint16_t umemory_address_t;
+typedef int16_t memory_address_t;
 
 /*
  * Based on code from several sources:
@@ -35,35 +39,69 @@ along with the Arduino Memory Library.  If not, see <http://www.gnu.org/licenses
  * http://www.nongnu.org/avr-libc/user-manual/malloc.html
  */
 
-extern unsigned int __data_start;
-extern unsigned int __data_end;
-extern unsigned int __bss_start;
-extern unsigned int __bss_end;
-extern unsigned int __heap_start;
+extern umemory_address_t __data_start;
+extern umemory_address_t __data_end;
+extern umemory_address_t __bss_start;
+extern umemory_address_t __bss_end;
+extern umemory_address_t __heap_start;
 extern void *__brkval;
+#else
+typedef uint32_t umemory_address_t;
+typedef int32_t memory_address_t;
 
-inline int16_t ram_size() {
-  return (int16_t) RAMEND - (int16_t) &__data_start;
+#define RAMEND 0
+#define SP 0
+#endif
+
+#ifdef __AVR__
+inline memory_address_t ram_size() {
+  return (memory_address_t) RAMEND - (memory_address_t) &__data_start;
 }
-inline int16_t data_size() {
-  return (int16_t) &__data_end - (int16_t) &__data_start;
+inline memory_address_t data_size() {
+  return (memory_address_t) &__data_end - (memory_address_t) &__data_start;
 }
-inline int16_t bss_size() {
-  return (int16_t) &__bss_end - (int16_t) &__bss_start;
+inline memory_address_t bss_size() {
+  return (memory_address_t) &__bss_end - (memory_address_t) &__bss_start;
 }
-inline int16_t heap_size() {
-  if((int16_t)__brkval==0) { // no heap
+inline memory_address_t heap_size() {
+  if((memory_address_t)__brkval==0) { // no heap
     return 0;
   }
-  return (int16_t) __brkval - (int16_t) &__heap_start;
+  return (memory_address_t) __brkval - (memory_address_t) &__heap_start;
 }
-inline int16_t stack_size() {
-  return (int16_t) RAMEND - (int16_t) SP;
+inline memory_address_t stack_size() {
+  return (memory_address_t) RAMEND - (memory_address_t) SP;
 }
-inline int16_t free_memory() {
-  if((int16_t)__brkval==0) { // no heap
-    return (int16_t) SP - (int16_t) &__bss_end;
+inline memory_address_t free_memory() {
+  if((memory_address_t)__brkval==0) { // no heap
+    return (memory_address_t) SP - (memory_address_t) &__bss_end;
   }
-  return (int16_t) SP - (int16_t) __brkval;
+  return (memory_address_t) SP - (memory_address_t) __brkval;
 }
+#else
+inline memory_address_t ram_size() { return 0; }
+inline memory_address_t data_size() { return 0; }
+inline memory_address_t bss_size() { return 0; }
+inline memory_address_t heap_size() { return 0; }
+inline memory_address_t stack_size() { return 0; }
+inline memory_address_t free_memory() {
+    /* Code from [Teensy 3 forum][1].
+     *
+     * [1]: https://forum.pjrc.com/threads/23256-Get-Free-Memory-for-Teensy-3-0?p=34242&viewfull=1#post34242 */
+    uint32_t stackTop;
+    uint32_t heapTop;
+
+    // Current position of the stack.
+    stackTop = (uint32_t) &stackTop;
+
+    // Current position of heap.
+    void* hTop = malloc(1);
+    heapTop = (uint32_t) hTop;
+    free(hTop);
+
+    // The difference is the free, available ram.
+    return stackTop - heapTop;
+}
+#endif
+
 #endif
