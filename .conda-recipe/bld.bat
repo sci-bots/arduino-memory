f6c28800c1d01c60772bce8322f69cbeaacb1334
Christian Fobel <christian@fobel.net>
2018-03-05 00:45:36 -0500

    build(conda): use platformio-helpers install path

--- .conda-recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal enableextensions
-md "%PREFIX%"\Library\include\Arduino
-endlocal
-xcopy /S /Y /I /Q "%SRC_DIR%"\include\Arduino\Memory "%PREFIX%"\Library\include\Arduino\Memory
-if errorlevel 1 exit 1
